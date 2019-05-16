7e268e903b58c55a699d2e3eca9653c347b5f449
JunC <826296634@qq.com>
2019-05-16 05:49:33 -0400

    impl OnReservice

--- tcp_pull/serv.cpp
+++ tcp_pull/serv.cpp
@@ -1,9 +1,11 @@
 #include <hpsocket/HPSocket.h>
+#include <hpsocket/common/GlobalDef.h>
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
+#include <vector>
 
-#define TCP_KEEPALIVE_TIME		(60 * 1000)
 /* Listener Class */
 class CListenerImpl : public CTcpPullServerListener
 {
@@ -12,16 +14,84 @@ public:
 	// 5. process network events
 	virtual EnHandleResult OnPrepareListen(ITcpServer* pSender, SOCKET soListen){
 		std::cout<< "OnPrepareListen...\n";
+
+		TCHAR szAddress[50];
+		int iAddressLen = sizeof(szAddress) / sizeof(TCHAR);
+		USHORT usPort;
+		pSender->GetListenAddress(szAddress, iAddressLen, usPort);
+		std::cout<< "servser addr: "<< szAddress << ", port: " << usPort <<"\n";
+
+		return HR_OK;
 	};
+
 	virtual EnHandleResult OnAccept(ITcpServer* pSender, CONNID dwConnID, UINT_PTR soClient){
-		std::cout<< "OnAccept...\n";
+		TCHAR szAddress[50];
+		int iAddressLen = sizeof(szAddress) / sizeof(TCHAR);
+		USHORT usPort;
+
+		pSender->GetRemoteAddress(dwConnID, szAddress, iAddressLen, usPort);
+		std::cout<< "get one connect: " << szAddress << ":" << usPort << "\n";
+
+		return HR_OK;
 	};
+
 	virtual EnHandleResult OnHandShake(ITcpServer* pSender, CONNID dwConnID){
-		std::cout<< "OnHandShake...\n";
+		//std::cout<< "OnHandShake...\n";
+		return HR_OK;
 	};
-	virtual EnHandleResult OnReceive(ITcpServer* pSender, CONNID dwConnID, int iLength){
-		std::cout<< "OnReceive...\n";
-	};
+
+        virtual EnHandleResult OnReceive(ITcpServer* pSender, CONNID dwConnID, int iLength);
+/*
+ *        virtual EnHandleResult OnReceive(ITcpServer* pSender, CONNID dwConnID, int iLength){
+ *                TPkgInfo* pInfo	= ::FindPkgInfo(pSender, dwConnID);
+ *                ITcpPullServer* pServer	= ITcpPullServer::FromS(pSender);
+ *
+ *                if(pInfo != nullptr)
+ *                {
+ *                        int required = pInfo->length;
+ *                        int remain = iLength;
+ *
+ *                        while(remain >= required)
+ *                        {
+ *                                remain -= required;
+ *                                CBufferPtr buffer(required);
+ *
+ *                                EnFetchResult result = pServer->Fetch(dwConnID, buffer, (int)buffer.Size());
+ *
+ *                                if(result == FR_OK)
+ *                                {
+ *                                        ::PostOnReceive(dwConnID, buffer, (int)buffer.Size());
+ *
+ *                                        if(pInfo->is_header)
+ *                                        {
+ *                                                TPkgHeader* pHeader = (TPkgHeader*)buffer.Ptr();
+ *#ifdef DEBUG
+ *                                                PRINTLN("(head) -> seq: %d, body_len: %d", pHeader->seq, pHeader->body_len);
+ *#endif
+ *                                                required = pHeader->body_len;
+ *                                        }
+ *                                        else
+ *                                        {
+ *#ifdef DEBUG
+ *                                                TPkgBody* pBody = (TPkgBody*)(BYTE*)buffer;
+ *                                                PRINTLN("(body) -> name: %s, age: %d, desc: %s", pBody->name, pBody->age, pBody->desc);
+ *#endif
+ *                                                required = sizeof(TPkgHeader);
+ *                                        }
+ *
+ *                                        pInfo->is_header = !pInfo->is_header;
+ *                                        pInfo->length	 = required;
+ *
+ *                                        if(!pSender->Send(dwConnID, buffer, (int)buffer.Size()))
+ *                                                return HR_ERROR;
+ *                                }
+ *                        }
+ *                }
+ *
+ *                return HR_OK;
+ *        };
+ */
+
 	virtual EnHandleResult OnSend(ITcpServer* pSender, CONNID dwConnID, const BYTE* pData, int iLength){
 		std::cout<< "OnSend...\n";
 	};
@@ -33,6 +103,30 @@ public:
 	};
 
 };
+
+EnHandleResult CListenerImpl::OnReceive(ITcpServer* pSender, CONNID dwConnID, int iLength){
+	std::vector<BYTE> vec;
+
+	ITcpPullServer* pServer = ITcpPullServer::FromS(pSender);
+	// package size 16
+	int required = 16;
+	int remain = iLength;
+
+	while(remain > 0){
+		BYTE buf[required];
+		if (required > remain) {
+			required = remain;
+		}
+		EnFetchResult result = pServer->Fetch(dwConnID, buf, required);
+		if(result == FR_OK) {
+			vec.insert(vec.end(), buf, buf+required);
+		}
+		remain -= required;
+	}
+	
+	std::string res(vec.begin(), vec.end());
+	std::cout<< res << std::endl;
+}
 
 int main(int argc, char* const argv[])
 {
